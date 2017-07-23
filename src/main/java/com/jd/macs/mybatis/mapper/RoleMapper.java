47af8fdcd9186725c47cd9f1180617582e305c77
Ma Changsheng <changshengm@oupeng.com>
2017-07-23 22:42:04 +0800

    add test case

--- src/main/java/com/jd/macs/mybatis/mapper/RoleMapper.java
+++ src/main/java/com/jd/macs/mybatis/mapper/RoleMapper.java
@@ -1,6 +1,9 @@
 package com.jd.macs.mybatis.mapper;
 
 import com.jd.macs.mybatis.po.Role;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * Created by machangsheng on 17/7/23.
@@ -10,4 +13,7 @@ public interface RoleMapper {
     Role getRole(Long id);
     int deleteRole(Long id);
     int insertRole(Role role);
+
+    @Select("select id, role_name as roleName, note from t_role where id = 7")
+    List<Role> getAllRoles();
 }
--- src/test/java/com/jd/macs/mybatis/mapper/RoleMapperTest.java
+++ src/test/java/com/jd/macs/mybatis/mapper/RoleMapperTest.java
@@ -1,7 +1,10 @@
 package com.jd.macs.mybatis.mapper;
 
 import com.jd.macs.mybatis.po.Role;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by machangsheng on 17/7/23.
@@ -17,5 +20,29 @@ public class RoleMapperTest extends BaseTest {
         RoleMapper roleMapper = getSqlSession().getMapper(RoleMapper.class);
 
         roleMapper.insertRole(role);
+    }
+
+    @Test
+    public void testDelete(){
+        RoleMapper roleMapper = getSqlSession().getMapper(RoleMapper.class);
+        roleMapper.deleteRole(3L);
+    }
+
+    @Test
+    public void testGetRole(){
+        RoleMapper roleMapper = getSqlSession().getMapper(RoleMapper.class);
+        Role role = roleMapper.getRole(7L);
+
+        Assert.assertEquals("admin", role.getRoleName());
+        Assert.assertEquals("super role", role.getNote());
+    }
+
+    @Test
+    public void testGetAllRoles(){
+        RoleMapper roleMapper = getSqlSession().getMapper(RoleMapper.class);
+        List<Role> roleList = roleMapper.getAllRoles();
+
+        Assert.assertEquals(1, roleList.size());
+        Assert.assertEquals("admin", roleList.get(0).getRoleName());
     }
 }
