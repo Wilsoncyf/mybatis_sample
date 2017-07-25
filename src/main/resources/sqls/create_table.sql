92e00b874a8291766a355fd2542da87b8f783c2b
Ma Changsheng <changshengm@oupeng.com>
2017-07-25 23:31:32 +0800

    add student

--- src/main/resources/sqls/create_table.sql
+++ src/main/resources/sqls/create_table.sql
@@ -17,3 +17,60 @@ CREATE TABLE `t_role` (
   `note` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+create table t_student (
+	id bigint primary key auto_increment not null,
+    cnname varchar(60) not null,
+    sex smallint not null,
+	note varchar(500)
+) engine=innodb default charset utf8;
+
+create table t_student_selfcard (
+	id bigint primary key auto_increment not null,
+    student_id bigint not null,
+    native varchar(60) not null,
+	issue_date date not null,
+    end_date date not null,
+	note varchar(500)
+) engine=innodb default charset utf8;
+
+create table t_student_lecture (
+	id bigint primary key auto_increment not null,
+    student_id bigint not null,
+    lecture_id bigint not null,
+	grade double(10,2) not null,
+	note varchar(500)
+) engine=innodb default charset utf8;
+
+create table t_lecture (
+	id bigint primary key auto_increment not null,
+    lecture_name varchar(100) not null,
+	note varchar(500)
+) engine=innodb default charset utf8;
+
+create table t_student_health_female (
+	id bigint(20) primary key auto_increment not null,
+	student_id bigint(20) not null,
+    check_date date not null,
+    heart varchar(60) not null,
+    liver varchar(60) not null,
+    spleen varchar(60) not null,
+    lung varchar(60) not null,
+    kidney varchar(60) not null,
+    uterus varchar(60) not null,
+	note varchar(500)
+) engine=innodb default charset utf8;
+
+create table t_student_health_male (
+	id bigint(20) primary key auto_increment not null,
+	student_id bigint(20) not null,
+    check_date date not null,
+    heart varchar(60) not null,
+    liver varchar(60) not null,
+    spleen varchar(60) not null,
+    lung varchar(60) not null,
+    kidney varchar(60) not null,
+    prostate varchar(60) not null,
+	note varchar(500)
+) engine=innodb default charset utf8;
